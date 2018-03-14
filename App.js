610ae3b5f9643ce6bc8a75df64bc8ffd1209dac6
mgol <golmarcin@gmail.com>
2018-03-14 23:03:19 +0100

    add some form logic

--- App.js
+++ App.js
@@ -4,17 +4,24 @@
 
 import React, { Component } from 'react';
 import {
+  Button,
   FlatList,
   ScrollView,
-  StyleSheet, View
+  StyleSheet, Text, View
 } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 import ActivityCard from "./src/components/activity-card";
 import ActivityForm from "./src/components/activity-form";
 
 type Props = {};
 
-export default class App extends Component<Props> {
+class App extends Component<Props> {
+  static navigationOptions = {
+    title: 'Networker'
+  };
+
   state = {
+    formVisible: false,
     activities: [
       {
         img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRb8hLheVssxMAPVJFe62-fxm2DTZCrbZdOJ8C5TMD48AwlTVnd',
@@ -26,18 +33,41 @@ export default class App extends Component<Props> {
   };
 
   render() {
-    return <View style={styles.container}>
-      <ActivityForm addActivity={form => { this.setState(state => ({...state, activities: [...state.activities, form]})); }}/>
-      {/*<FlatList
-        data={activities}
-        renderItem={({item}) => <ActivityCard {...item}/>}
-      />*/}
+    return <ScrollView style={styles.container}>
+      <Button
+        style={{margin: 5}}
+        onPress={() => this.props.navigation.navigate('Profile')}
+        title={'Go to Profile'}
+      />
+      {!this.state.formVisible && <Button
+        onPress={() => this.setState(state => ({...state, formVisible: !this.state.formVisible}))}
+        title={'Add new activity'}
+      />}
+      {this.state.formVisible && <ActivityForm addActivity={form => { this.setState(state => ({ ...state, activities: [...state.activities, form], formVisible: false })); }}/>}
       <FlatList
         data={this.state.activities}
         keyExtractor={(item, index) => item.uuid}
         renderItem={({item}) => <ActivityCard {...item} />}
       />
-    </View>;
+    </ScrollView>;
+  }
+}
+
+class Profile extends Component<Props> {
+  static navigationOptions = {
+    title: 'Profile'
+  };
+
+  render () {
+    return (
+      <View>
+        <Button
+          onPress={() => this.props.navigation.navigate('Home')}
+          title={'Go Home'}
+        />
+        <Text> Ima profile! </Text>
+      </View>
+    );
   }
 }
 
@@ -46,4 +76,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
     flex: 1
   }
+});
+
+export default StackNavigator({
+  Home: {
+    screen: App
+  },
+  Profile: {
+    screen: Profile
+  }
+},{
+  initialRouteName: 'Home'
 });
--- src/components/activity-form.js
+++ src/components/activity-form.js
@@ -7,7 +7,8 @@ class ActivityForm extends Component {
     name: 'Some cool texts',
     description: 'some description placeholder',
     category: 1,
-    uuid: uuid()
+    uuid: uuid(),
+    pickerVisible: false
   };
 
   /*
@@ -23,51 +24,59 @@ class ActivityForm extends Component {
     return (
       <View style={styles.container}>
         <View>
-          <Text style={styles.header}>Create new activity </Text>
-          <View style={{margin: 5}}>
+          {!this.state.pickerVisible && <View>
+            <Text style={styles.header}>Create new activity </Text>
+            <View style={{margin: 5}}>
+              <Text style={styles.label}>Name</Text>
+              <TextInput
+                style={styles.input}
+                onChangeText={(name) => this.setState(state => ({...state, name}))}
+                value={this.state.text}
+                autoCorrect={false}
+              />
+            </View>
+            <View style={{margin: 5}}>
+              <Text style={styles.label}>Description</Text>
+              <TextInput
+                style={styles.input}
+                multiline={true}
+                onChangeText={(description) => this.setState(state => ({...state, description}))}
+                value={this.state.text}
+                autoCorrect={false}
+              />
+            </View>
+          </View>}
+          <View>
+            { !this.state.pickerVisible && <Button
+              title={'Choose category'}
+              onPress={() => this.setState(state => ({...state, pickerVisible: true}))}
+            /> }
+            {this.state.pickerVisible && <Picker
+              style={{borderColor: 'black', borderWidth: 1, backgroundColor: 'red'}}
+              selectedValue={this.state.category}
+              onValueChange={(itemValue, itemIndex) => this.setState( state => ({...state, category: itemValue, pickerVisible: false}))}
+            >
+              <Picker.Item label={'Sports'} value={12} />
+              <Picker.Item label={'Music'} value={13} />
+              <Picker.Item label={'Bowling'} value={14} />
+              <Picker.Item label={'Professional alcoholism'} value={15} />
+              <Picker.Item label={'Biking'} value={16} />
+              <Picker.Item label={'Night racing'} value={17} />
+            </Picker>}
+          <View style={{borderWidth: 2, borderColor: 'black', padding: 20}}>
+            <Text style={styles.header}> Preview of activity </Text>
             <Text style={styles.label}>Name</Text>
-            <TextInput
-              style={styles.input}
-              onChangeText={(name) => this.setState(state => ({...state, name}))}
-              value={this.state.text}
-              autoCorrect={false}
-            />
-          </View>
-          <View style={{margin: 5}}>
+            <Text>{this.state.name}</Text>
+
             <Text style={styles.label}>Description</Text>
-            <TextInput
-              style={styles.input}
-              multiline={true}
-              onChangeText={(description) => this.setState(state => ({...state, description}))}
-              value={this.state.text}
-              autoCorrect={false}
-            />
+            <Text>{this.state.description}</Text>
+
+            <Text style={styles.label}>Category</Text>
+            <Text>{this.state.category}</Text>
           </View>
-          {/*<Picker
-            style={{borderColor: 'black', borderWidth: 5}}
-            selectedValue={this.state.category}
-            onValueChange={(itemValue, itemIndex) => this.setState( state => ({...state, category: itemValue}))}
-          >
-            <Picker.Item label={'Sports'} value={12} />
-            <Picker.Item label={'Music'} value={13} />
-            <Picker.Item label={'Bowling'} value={14} />
-            <Picker.Item label={'Professional alcoholism'} value={15} />
-            <Picker.Item label={'Biking'} value={16} />
-            <Picker.Item label={'Night racing'} value={17} />
-          </Picker>*/}
-        </View>
-        <View style={{borderWidth: 2, borderColor: 'black', padding: 20}}>
-          <Text style={styles.header}> Preview of activity </Text>
-          <Text style={styles.label}>Name</Text>
-          <Text>{this.state.name}</Text>
-
-          <Text style={styles.label}>Description</Text>
-          <Text>{this.state.description}</Text>
-
-          <Text style={styles.label}>Category</Text>
-          <Text>{this.state.category}</Text>
         </View>
         <Button style={{marginTop: 5}} title={'Add activity'} onPress={() => { this.props.addActivity(this.state); this.setState(state => ({...state, uuid: uuid()})) }}/>
+        </View>
       </View>
     );
   }
@@ -76,9 +85,11 @@ class ActivityForm extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    margin: 5
+    margin: 5,
+    height: 600
   },
   input: {
+    margin: 5
   },
   label: {
     color: '#575757',
