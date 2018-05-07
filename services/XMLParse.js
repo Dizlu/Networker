11f804d2cc8d137d20e0110cf2c728b53df49316
Marcin Gol <golmarcin@gmail.com>
2018-05-07 23:23:42 +0200

    change default xml source of events

--- services/XMLParse.js
+++ services/XMLParse.js
@@ -3,7 +3,7 @@ import XMLFetch from "./XMLFetch";
 import * as fastXmlParser from "fast-xml-parser";
 
 export default async (link: string) => {
-  const data = await XMLFetch('https://lublin.eu/rss/pl/66/2.xml');
+  const data = await XMLFetch('https://lublin.eu/rss/pl/41/2.xml');
 
   let parsed = fastXmlParser.parse(data);
 
