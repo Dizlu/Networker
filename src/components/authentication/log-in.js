de34f5acd36a1b0c6d84ce7e276ff8210c7cfe85
Marcin Gol <golmarcin@gmail.com>
2018-05-26 14:32:38 +0200

    add support for logging with email

--- src/components/authentication/log-in.js
+++ src/components/authentication/log-in.js
@@ -5,6 +5,19 @@ import { Button, Caption, Title, Tile, TextInput, Text } from "@shoutem/ui";
 //import { connect } from "react-redux";
 
 export class LogIn extends Component {
+  unsubscriber = null;
+  state = {
+    login: "",
+    password: "",
+    uid: null
+  };
+
+  componentDidMount() {
+    this.unsubscriber = firebase.auth().onAuthStateChanged(user => {
+      this.setState(user);
+    });
+  }
+
   logInAnonymously() {
     console.log("logging...");
     firebase
@@ -20,38 +33,67 @@ export class LogIn extends Component {
       });
   }
 
+  loginWithEmail(state) {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(state.login, state.password)
+      .then(res => {
+        this.setState(state => ({
+          ...state,
+          uid: res.uid
+        }));
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
-    return (
-      <View>
-        <Button>
-          <Caption
-            style={{ margin: 15, padding: 15 }}
-            onPress={() => this.logInAnonymously()}
-          >
-            Log in annonymously here!
-          </Caption>
-        </Button>
-        <Tile style={{ margin: 10 }}>
-          <Title styleName={"h-center"} style={{ margin: 25 }}>
-            Log in here
-          </Title>
-          <TextInput
-            placeholder={"Username or email"}
-            onChangeText={text => console.log(text)}
-            autoCorrect={false}
-          />
-          <TextInput
-            placeholder={"Password"}
-            onChangeText={text => console.log(text)}
-            autoCorrect={false}
-            secureTextEntry
-          />
-          <Button styleName={"dark"}>
-            <Text>Log in!</Text>
+    const isLogged = this.state.uid;
+    if (!isLogged) {
+      return (
+        <View>
+          <Button>
+            <Caption
+              style={{ margin: 15, padding: 15 }}
+              onPress={() => this.logInAnonymously()}
+            >
+              Log in annonymously here!
+            </Caption>
           </Button>
-        </Tile>
-      </View>
-    );
+          <Tile style={{ margin: 10 }}>
+            <Title styleName={"h-center"} style={{ margin: 25 }}>
+              Log in here
+            </Title>
+            <TextInput
+              placeholder={"Username or email"}
+              onChangeText={text =>
+                this.setState(state => ({ ...state, login: text }))
+              }
+              autoCorrect={false}
+            />
+            <TextInput
+              placeholder={"Password"}
+              onChangeText={text =>
+                this.setState(state => ({ ...state, password: text }))
+              }
+              autoCorrect={false}
+              secureTextEntry
+            />
+            <Button
+              styleName={"dark"}
+              onPress={() => this.loginWithEmail(this.state)}
+            >
+              <Text>Log in!</Text>
+            </Button>
+          </Tile>
+        </View>
+      );
+    } else if (this.state.uid) {
+      return (
+        <View>
+          <Title>Hello dear user!</Title>
+        </View>
+      );
+    }
   }
 }
 /* 
